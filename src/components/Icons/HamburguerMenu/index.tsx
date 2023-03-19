654e2688d2c569ec17a3b8b36af862bee9887b08
Eduardoooxd <ecouto93@gmail.com>
2023-03-19 20:00:59 +0000

    feat: Created Navbar for page

---
+++ src/components/Icons/HamburguerMenu/index.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent } from 'react';
+import { RxCross1 } from 'react-icons/rx';
+
+interface HamburgerMenuProps {
+    isOpen: boolean;
+}
+
+export const OpenHamburgerMenu: FunctionComponent<HamburgerMenuProps> = ({ isOpen }) => {
+    return (
+        <svg
+            className={`h-6 w-6 ${isOpen ? 'hidden' : 'block'}`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+        >
+            <path
+                fillRule="evenodd"
+                d="M2 4h16a1 1 0 0 1 0 2H2a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H2a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H2a1 1 0 0 1 0-2z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+};
+
+export const ClosedHamburgerMenu: FunctionComponent<HamburgerMenuProps> = ({ isOpen }) => {
+    return isOpen ? <RxCross1 size={'1.5rem'} /> : null;
+};
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,39 @@
 'use client';
 
 import LogoImage from '@/images/logo/orun-logo.png';
-import { commutersSans } from '@/lib/fontLoader';
 import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { Dispatch, FunctionComponent, SetStateAction, useEffect, useState } from 'react';
 
+import { useScrollBlock } from '@/hooks/useScrollBlock';
+import { commutersSans } from '@/lib/fontLoader';
+import { Transition } from '@headlessui/react';
+import Link from 'next/link';
 import { CiSearch } from 'react-icons/ci';
 import { RxPerson } from 'react-icons/rx';
 import Bag from '../Icons/Bag';
+import { ClosedHamburgerMenu, OpenHamburgerMenu } from '../Icons/HamburguerMenu';
+import NavbarLink, { NavbarLinkProps } from './NavbarLink';
+
+const NAVBAR_LINKS: NavbarLinkProps[] = [
+    {
+        text: 'Men',
+    },
+    {
+        text: 'Categories',
+    },
+    {
+        text: 'About us',
+    },
+    {
+        text: 'Contact us',
+    },
+];
 
 export default function Navbar() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState<boolean>(false);
 
-    const stylesOnScroll = isScrolled ? 'bg-white shadow-md' : 'bg-transparent';
+    const stylesOnScroll = isScrolled || isMobileMenuOpen ? 'bg-white shadow-md' : 'bg-transparent';
 
     useEffect(() => {
         const handleScroll = () => {
@@ -31,21 +51,34 @@ export default function Navbar() {
 
     return (
         <nav
-            className={`fixed top-0 flex h-20 items-center justify-evenly w-full transition duration-300 ease-in-out z-50 ${stylesOnScroll}`}
+            className={`fixed top-0 z-50 flex min-h-[5rem] w-full flex-col justify-evenly  transition duration-300 ease-in-out ${stylesOnScroll}`}
         >
-            <div className="flex gap-10 items-center">
+            <MobileNavBar
+                isMobileMenuOpen={isMobileMenuOpen}
+                setIsMobileMenuOpen={setIsMobileMenuOpen}
+            />
+
+            <DesktopNavBar />
+        </nav>
+    );
+}
+
+const DesktopNavBar: FunctionComponent = () => {
+    return (
+        <div className="container mx-auto hidden h-20 w-full items-center justify-between p-4 sm:p-6 md:flex">
+            <div className="flex items-center gap-6 lg:gap-10">
                 <Link
                     className={`${commutersSans.variable} font-commutersSans font-extralight`}
                     href=""
                 >
                     MEN
                 </Link>
-                <a
+                <Link
                     className={`${commutersSans.variable} font-commutersSans font-extralight`}
                     href=""
                 >
-                    WOMEN
-                </a>
+                    CATEGORIES
+                </Link>
 
                 <Link
                     className={`${commutersSans.variable} font-commutersSans font-extralight`}
@@ -56,10 +89,10 @@ export default function Navbar() {
             </div>
             <div>
                 <Link href="">
-                    <Image priority src={LogoImage} alt="logo" width={200} height={40} />
+                    <Image priority src={LogoImage} alt="logo" width={150} height={30} />
                 </Link>
             </div>
-            <div className="flex gap-10 items-center">
+            <div className="flex items-center gap-10">
                 <Link
                     className={`${commutersSans.variable} font-commutersSans font-extralight`}
                     href=""
@@ -73,6 +106,62 @@ export default function Navbar() {
 
                 <Bag />
             </div>
-        </nav>
+        </div>
     );
+};
+
+interface NobileNavBarProps {
+    isMobileMenuOpen: boolean;
+    setIsMobileMenuOpen: Dispatch<SetStateAction<boolean>>;
 }
+
+const MobileNavBar: FunctionComponent<NobileNavBarProps> = ({
+    isMobileMenuOpen,
+    setIsMobileMenuOpen,
+}) => {
+    const [blockScroll, allowScroll] = useScrollBlock();
+    isMobileMenuOpen ? blockScroll() : allowScroll();
+
+    return (
+        <>
+            <div className="container mx-auto flex h-20 w-full items-center justify-between p-4 sm:p-6 md:hidden">
+                <div>
+                    <Link href="">
+                        <Image priority src={LogoImage} alt="logo" width={150} height={30} />
+                    </Link>
+                </div>
+                <button
+                    type="button"
+                    onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                    className="relative h-10 w-10 rounded p-2 text-black transition duration-150 ease-in-out hover:bg-gray-900 hover:text-white focus:outline-none"
+                    aria-controls="mobile-menu"
+                    aria-expanded="false"
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <OpenHamburgerMenu isOpen={isMobileMenuOpen} />
+                    <ClosedHamburgerMenu isOpen={isMobileMenuOpen} />
+                </button>
+            </div>
+
+            <Transition
+                show={isMobileMenuOpen}
+                enter="transition ease-out duration-100 transform"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="transition ease-in duration-75 transform"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+            >
+                {() => (
+                    <div className="h-[calc(100dvh-5rem)] p-4 sm:p-6 md:hidden">
+                        <div className="flex flex-col">
+                            {NAVBAR_LINKS.map((navBarLink, index) => (
+                                <NavbarLink key={index} {...navBarLink} />
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </Transition>
+        </>
+    );
+};
