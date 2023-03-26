e3a0dbc2c421bb3bec9543fd900cbc3fdef5f6e2
Eduardoooxd <ecouto93@gmail.com>
2023-03-26 13:11:28 +0100

    chore: Adding sharp package to improve image optimization and fixing relative paths for fonts

--- lib/fontLoader.tsx
+++ lib/fontLoader.tsx
@@ -3,12 +3,12 @@ import localFont from 'next/font/local';
 export const commutersSans = localFont({
     src: [
         {
-            path: '/../../public/assets/fonts/commuterssans-regular.otf',
+            path: '/../public/assets/fonts/commuterssans-regular.otf',
             weight: '400',
             style: 'normal',
         },
         {
-            path: '/../../public/assets/fonts/commuterssans-extralight.otf',
+            path: '/../public/assets/fonts/commuterssans-extralight.otf',
             weight: '200',
             style: 'normal',
         },
@@ -17,6 +17,6 @@ export const commutersSans = localFont({
 });
 
 export const futuraPTLight = localFont({
-    src: '/../../public/assets/fonts/futura-pt-light.otf',
+    src: '/../public/assets/fonts/futura-pt-light.otf',
     variable: '--font-futuraPTLight',
 });
