0b6fed1119a7e2fdf35d4f5a0ed55cbb12d1102d
Eduardoooxd <ecouto93@gmail.com>
2023-04-08 18:12:28 +0100

    chore: added clerk dependecy for auth

--- TODO.md
+++ TODO.md
@@ -9,3 +9,7 @@ Implement analytics
 Create page for return policy
 Research how to track order
 Research best way to handle storage and handle info about order shipping
+Add Suspense and Error handling
+
+Fix:
+Fix font family does not have - / letters
--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.clerk.dev',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
