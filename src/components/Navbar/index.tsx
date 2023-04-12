a3a1847cc1045d75ba9d3f76217012633ff83511
Eduardoooxd <ecouto93@gmail.com>
2023-04-12 19:57:00 +0100

    🚀 chore(package.json): add 'turbo' script 🎨 style(Mobile.tsx): move MobileNavBar component to its own file 🎨 style(useLockBody.tsx): create a new file for useLockBody hook The 'turbo' script has been added to the package.json file to allow for faster development with Next.js' experimental Turbo Mode. The MobileNavBar component has been moved to its own file to improve code organization. The useLockBody hook has been moved to its own file to improve code organization.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,29 +4,18 @@ import MiniLogoImage from '@/images/logo/logo-mini.png';
 import Image from 'next/image';
 import './styles.css';
 
-import { SignIn, useClerk, UserButton, useUser } from '@clerk/nextjs/app-beta/client';
+import { useClerk, useUser } from '@clerk/nextjs/app-beta/client';
 
-import {
-    Dispatch,
-    FunctionComponent,
-    memo,
-    SetStateAction,
-    Suspense,
-    useCallback,
-    useEffect,
-    useState,
-} from 'react';
+import { FunctionComponent, memo, Suspense, useCallback, useEffect, useState } from 'react';
 
 import { commutersSans } from '@/lib/fontLoader';
-import { Transition } from '@headlessui/react';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { CiSearch } from 'react-icons/ci';
 import { RxPerson } from 'react-icons/rx';
 import Bag from '../Icons/Bag';
-import { ClosedHamburgerMenu, OpenHamburgerMenu } from '../Icons/HamburguerMenu';
-import NavbarLink, { NavbarLinkProps } from './NavbarLink';
+import { MobileNavBar } from './Mobile';
 
 export default function Navbar() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -222,150 +211,6 @@ const DropdownMenuUserOptions: FunctionComponent = () => {
                     Sign out
                 </button>
             </DropdownMenu.Item>
-        </>
-    );
-};
-
-const NAVBAR_LINKS: NavbarLinkProps[] = [
-    {
-        text: 'Categories',
-    },
-    {
-        text: 'About us',
-    },
-    {
-        text: 'Contact us',
-    },
-];
-
-interface NobileNavBarProps {
-    isMobileMenuOpen: boolean;
-    setIsMobileMenuOpen: Dispatch<SetStateAction<boolean>>;
-}
-
-const MobileNavBar: FunctionComponent<NobileNavBarProps> = ({
-    isMobileMenuOpen,
-    setIsMobileMenuOpen,
-}) => {
-    return (
-        <>
-            <div className="container mx-auto flex h-20 w-full items-center justify-between p-4 sm:p-6 md:hidden">
-                <div>
-                    <Link href="/">
-                        <Image priority src={MiniLogoImage} alt="logo" width={90} height={20} />
-                    </Link>
-                </div>
-                <button
-                    type="button"
-                    onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-                    className="relative h-10 w-10 rounded p-2 text-black transition duration-150 ease-in-out hover:bg-gray-900 hover:text-white focus:outline-none"
-                    aria-controls="mobile-menu"
-                    aria-expanded="false"
-                >
-                    <span className="sr-only">Open main menu</span>
-                    <OpenHamburgerMenu isOpen={isMobileMenuOpen} />
-                    <ClosedHamburgerMenu isOpen={isMobileMenuOpen} />
-                </button>
-            </div>
-
-            <Transition
-                show={isMobileMenuOpen}
-                enter="transition ease-out duration-100 transform"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="transition ease-in duration-75 transform"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-            >
-                {() => (
-                    <div className="h-[calc(100dvh-5rem)] p-4 sm:p-6 md:hidden">
-                        <div className="flex flex-col gap-4">
-                            <MobileMenuUserOptions />
-
-                            {NAVBAR_LINKS.map((navBarLink, index) => (
-                                <NavbarLink key={index} {...navBarLink} />
-                            ))}
-                        </div>
-                    </div>
-                )}
-            </Transition>
-        </>
-    );
-};
-
-const MobileMenuUserOptions: FunctionComponent = ({}) => {
-    const { isLoaded, isSignedIn } = useUser();
-
-    if (!isLoaded || !isSignedIn) {
-        return <MobileSignInDropDown />;
-    }
-
-    return (
-        <UserButton
-            showName
-            appearance={{
-                elements: {
-                    rootBox: 'w-full',
-                    userButtonBox: 'w-full justify-between',
-                    userButtonOuterIdentifier: `${commutersSans.variable} font-commutersSans font-extralight uppercase text-base`,
-                },
-            }}
-        />
-    );
-};
-
-const MobileSignInDropDown: FunctionComponent = () => {
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-
-    const handleDropdownToggle = useCallback(() => {
-        setDropdownOpen((prevState) => !prevState);
-    }, []);
-
-    const SingInOptions = [
-        {
-            text: 'Sign in',
-            component: (
-                <SignIn
-                    signUpUrl="/sign-up"
-                    afterSignInUrl="/"
-                    appearance={{
-                        elements: {
-                            rootBox: 'bg-transparent w-full',
-                            card: 'py-2 px-0 shadow-none max-w-sm mx-auto w-full',
-                            header: 'hidden',
-                        },
-                    }}
-                />
-            ),
-        },
-    ];
-
-    return (
-        <>
-            {SingInOptions.map((option, index) => (
-                <DropdownMenu.Root key={index} open={dropdownOpen} onOpenChange={setDropdownOpen}>
-                    <DropdownMenu.Trigger asChild onClick={handleDropdownToggle}>
-                        <button
-                            aria-label={option.text}
-                            className={`${commutersSans.variable} text-left font-commutersSans font-extralight uppercase`}
-                        >
-                            {option.text}
-                        </button>
-                    </DropdownMenu.Trigger>
-
-                    <DropdownMenu.Portal>
-                        <DropdownMenu.Content
-                            align="start"
-                            side="left"
-                            className={`absolute left-0 z-[60] mt-8 flex w-80 bg-transparent`}
-                        >
-                            <DropdownMenu.Item className="w-full">
-                                {option.component}
-                            </DropdownMenu.Item>
-                        </DropdownMenu.Content>
-                    </DropdownMenu.Portal>
-                </DropdownMenu.Root>
-            ))}
         </>
     );
 };
