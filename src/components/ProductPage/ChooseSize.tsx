48839397ca5c6b3a061c1593d7f0195cb99347e8
Eduardoooxd <ecouto93@gmail.com>
2023-04-17 19:56:02 +0100

    🎨 style(ProductPage): add ChooseSize component and update styles ✨ feat(ProductPage): add ChooseSize component to allow users to select product size The ChooseSize component was added to the ProductPage component to allow users to select the size of the product they want to purchase. The styles were also updated to add an underline to the size text when the dropdown is open and to rotate the down arrow icon when the dropdown is open.

---
+++ src/components/ProductPage/ChooseSize.tsx
@@ -0,0 +1,96 @@
+'use client';
+import { futuraPTLight } from '@/lib/fontLoader';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { AnimatePresence, motion, useAnimationControls } from 'framer-motion';
+import { FunctionComponent, useEffect, useState } from 'react';
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+import './styles.css';
+
+interface ChooseSizeProps {
+    sizes: string[];
+}
+
+const ChooseSize: FunctionComponent<ChooseSizeProps> = ({ sizes }) => {
+    const [open, setOpen] = useState(false);
+    const controls = useAnimationControls();
+
+    useEffect(() => {
+        if (open) {
+            controls.start('open');
+        }
+    }, [controls, open]);
+
+    return (
+        <section className="my-4 py-4">
+            <DropdownMenu.Root open={open} onOpenChange={setOpen}>
+                <DropdownMenu.Trigger asChild>
+                    <button
+                        type="button"
+                        aria-label="Open size selector"
+                        className="chooseSizeButton group flex w-full cursor-pointer items-center justify-between focus-visible:outline-none"
+                    >
+                        <h4
+                            className={`${futuraPTLight.variable} font-futuraPTLight text-sm uppercase`}
+                        >
+                            Size
+                        </h4>
+
+                        <div className="flex items-center gap-x-2 ">
+                            <h4
+                                className={`${futuraPTLight.variable} sizeText font-futuraPTLight text-sm uppercase transition active:underline group-hover:underline`}
+                            >
+                                Choose size
+                            </h4>
+                            <MdOutlineKeyboardArrowDown className="downIcon h-9 w-9 transition" />
+                        </div>
+                    </button>
+                </DropdownMenu.Trigger>
+
+                <AnimatePresence>
+                    {open && (
+                        <DropdownMenu.Portal forceMount className="w-full">
+                            <DropdownMenu.Content
+                                align="start"
+                                className="mt-1 w-full overflow-hidden bg-white/75 text-left shadow backdrop-blur"
+                                asChild
+                            >
+                                <motion.div
+                                    initial="closed"
+                                    animate={controls}
+                                    exit="closed"
+                                    variants={{
+                                        open: {
+                                            translateY: '100%',
+                                            transition: {
+                                                ease: 'easeOut',
+                                                duration: 0.1,
+                                            },
+                                        },
+                                        closed: {
+                                            translateY: 0,
+                                            transition: {
+                                                ease: 'easeIn',
+                                                duration: 0.2,
+                                            },
+                                        },
+                                    }}
+                                >
+                                    {sizes.map((size) => (
+                                        <DropdownMenu.Item
+                                            className={`${futuraPTLight.variable} font-futuraPTLight text-sm uppercase`}
+                                            key={size}
+                                        >
+                                            {size}
+                                        </DropdownMenu.Item>
+                                    ))}
+                                </motion.div>
+                            </DropdownMenu.Content>
+                        </DropdownMenu.Portal>
+                    )}
+                </AnimatePresence>
+            </DropdownMenu.Root>
+        </section>
+    );
+};
+
+export default ChooseSize;
--- src/components/ProductPage/index.tsx
+++ src/components/ProductPage/index.tsx
@@ -1,7 +1,8 @@
-import { futuraPTLight } from '@/lib/fontLoader';
+import { commutersSans, futuraPTLight } from '@/lib/fontLoader';
 import { Product } from '@/lib/product';
 import Image from 'next/image';
 import { FunctionComponent } from 'react';
+import ChooseSize from './ChooseSize';
 import ProductsCarousel from './ProductsCarousel';
 
 interface ProductPageProps {
@@ -56,7 +57,7 @@ interface ProductDescriptionProps {
 }
 
 const ProductDescription: FunctionComponent<ProductDescriptionProps> = ({ product }) => {
-    const { title, price, description, category } = product;
+    const { title, price, description, category, sizes } = product;
 
     return (
         <div className="px-4 lg:px-24">
@@ -68,11 +69,18 @@ const ProductDescription: FunctionComponent<ProductDescriptionProps> = ({ produc
                     >
                         {title}
                     </h1>
-                    <p
-                        className={`${futuraPTLight.variable} mt-4 font-futuraPTLight text-base font-bold text-black`}
-                    >
-                        {price}&nbsp;EUR
-                    </p>
+                    <div className="flex w-full justify-between">
+                        <p
+                            className={`${futuraPTLight.variable} mt-4 font-futuraPTLight text-base font-bold text-black`}
+                        >
+                            {price}&nbsp;EUR
+                        </p>
+                        <p
+                            className={`${futuraPTLight.variable} mt-4 font-futuraPTLight text-base font-extrabold uppercase text-black`}
+                        >
+                            {category}
+                        </p>
+                    </div>
                 </header>
                 <hr className="border-t border-black" />
                 {/** Section to Description */}
@@ -83,36 +91,7 @@ const ProductDescription: FunctionComponent<ProductDescriptionProps> = ({ produc
                 </section>
                 <hr className="border-t border-black" />
                 {/** Section to chose Size */}
-                <section className="my-4 py-4">
-                    <button
-                        type="button"
-                        aria-label="Open size selector"
-                        className="flex w-full cursor-pointer justify-between"
-                        disabled
-                    >
-                        <h4
-                            className={`${futuraPTLight.variable} font-futuraPTLight text-sm uppercase`}
-                        >
-                            Size
-                        </h4>
-
-                        <div className="flex items-center gap-x-2 ">
-                            <h4
-                                className={`${futuraPTLight.variable} font-futuraPTLight text-sm uppercase`}
-                            >
-                                Choose size
-                            </h4>
-                            <svg
-                                data-name="Layer 1"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 8 4.71"
-                                className="h-2 w-2 transition-transform duration-200"
-                            >
-                                <path d="M7.29 0L4 3.29.71 0 0 .71l4 4 4-4L7.29 0z"></path>
-                            </svg>
-                        </div>
-                    </button>
-                </section>
+                <ChooseSize sizes={sizes} />
                 <hr className="border-t border-black" />
                 {/** Section for button */}
                 <ProductContactForm product={product} />
@@ -169,7 +148,12 @@ const ProductContactForm: FunctionComponent<ProductContactFormProps> = ({ produc
     };
 
     return (
-        <section className="my-4 py-4">
+        <section className="my-4 flex flex-col gap-2 py-4">
+            <span
+                className={`text-xs capitalize text-gray-500  ${commutersSans.variable} font-commutersSans font-extralight uppercase`}
+            >
+                Online shop coming soon
+            </span>
             <a
                 className={`${futuraPTLight.variable} block w-full bg-black p-4 text-center font-futuraPTLight font-bold uppercase text-white`}
                 href={`mailto:${
