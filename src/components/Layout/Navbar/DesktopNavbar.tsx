17ec9ee271052253a80fa9792265f90af454f47c
Eduardoooxd <ecouto93@gmail.com>
2023-04-17 12:23:55 +0100

    🎨 style(DesktopNavbar.tsx): remove Bag component and adjust layout The Bag component was removed from the DesktopNavbar component. The layout was adjusted to remove the gap between the logo and the navigation links. The search icon was moved to the left of the locale switcher component.

--- src/components/Layout/Navbar/DesktopNavbar.tsx
+++ src/components/Layout/Navbar/DesktopNavbar.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { FunctionComponent } from 'react';
 import { CiSearch } from 'react-icons/ci';
-import Bag from '../../Icons/Bag';
 import LocaleSwitcher from './LocaleSwitcher';
 import NavbarLink, { NavbarLinkProps } from './NavbarLink';
 
@@ -14,24 +13,24 @@ interface DesktopNavBarProps {
 export const DesktopNavBar: FunctionComponent<DesktopNavBarProps> = ({ links }) => {
     return (
         <div className="container mx-auto flex h-20 w-full items-center justify-between p-4 sm:p-6">
-            <div className="flex items-center gap-6 lg:gap-10">
-                <Link href="/">
-                    <Image priority src={MiniLogoImage} alt="logo" width={100} height={20} />
-                </Link>
-
+            <div className="flex  flex-1 items-center gap-6 lg:gap-10">
                 {links.map((navBarLink, index) => (
                     <NavbarLink key={index} {...navBarLink} />
                 ))}
             </div>
-            <div className="flex items-center gap-10">
+            <div className="flex  items-center gap-6 lg:gap-10 ">
+                <Link href="/">
+                    <Image priority src={MiniLogoImage} alt="logo" width={100} height={20} />
+                </Link>
+            </div>
+            <div className="flex flex-1 items-center justify-end gap-10">
+                {/* 
+                    <div className="grid aspect-square w-12 place-items-center">{<User /> }</div>
+                    <div className="grid aspect-square w-12 place-items-center">{<Bag /> }</div>
+                */}
                 <div className="grid aspect-square w-12 place-items-center">
                     <CiSearch size={'1.5rem'} />
                 </div>
-
-                <div className="grid aspect-square w-12 place-items-center">
-                    <Bag />
-                </div>
-
                 <LocaleSwitcher />
             </div>
         </div>
