48a0379fac57365f800bc96c20e028d8bfc9ad1c
Eduardoooxd <ecouto93@gmail.com>
2023-04-18 18:24:48 +0100

    🎨 style(fonts): replace otf font files with ttf font files The otf font files for Commuters Sans Regular and Commuters Sans ExtraLight have been removed and replaced with ttf font files. This change was made to improve compatibility with different operating systems and browsers. The fontLoader.tsx file has been updated to reflect the new file names.

--- src/lib/fontLoader.tsx
+++ src/lib/fontLoader.tsx
@@ -3,12 +3,12 @@ import localFont from 'next/font/local';
 export const commutersSans = localFont({
     src: [
         {
-            path: '/../../public/assets/fonts/commuterssans-regular.otf',
+            path: '/../../public/assets/fonts/commuterssans-regular.ttf',
             weight: '400',
             style: 'normal',
         },
         {
-            path: '/../../public/assets/fonts/commuterssans-extralight.otf',
+            path: '/../../public/assets/fonts/commuterssans-extralight.ttf',
             weight: '200',
             style: 'normal',
         },
