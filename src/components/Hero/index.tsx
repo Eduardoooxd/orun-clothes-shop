5e62ee150924ffd0bbdd95dea271ba781ec67958
Eduardoooxd <ecouto93@gmail.com>
2023-04-19 18:27:44 +0100

    🎨 style(Hero): add responsive background image and logo The Hero component now uses a different background image and logo for mobile devices. The useIsMobile hook is used to determine if the device is a mobile device, and the appropriate image is loaded based on that. The background image is also adjusted to better fit the mobile screen size.

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,23 +1,32 @@
+'use client';
+import useIsMobile from '@/hooks/useIsMobile';
 import LogoWhiteAlternativeImage from '@/images/logo/logo-white-small-alternative.png';
+import LogoWhiteImage from '@/images/logo/logo-white.png';
+
 import BackgroundImage from '@/images/main_background.webp';
+import BackgroundImageMobile from '@/images/main_background_mobile.webp';
 
 import Image from 'next/image';
 
 export default function Hero() {
+    const isMobile = useIsMobile();
+
     return (
         <section className="relative mt-[-5rem] min-h-screen w-full">
             <Image
                 priority
-                src={BackgroundImage}
+                src={isMobile ? BackgroundImageMobile : BackgroundImage}
                 alt="background"
                 fill
+                quality={100}
                 className="z-[-10]"
-                style={{ objectFit: 'cover', objectPosition: '25%' }}
+                style={{ objectFit: 'cover', objectPosition: '20%' }}
             />
+
             <div className={`flex min-h-screen w-full items-center justify-center`}>
                 <Image
                     priority
-                    src={LogoWhiteAlternativeImage}
+                    src={isMobile ? LogoWhiteImage : LogoWhiteAlternativeImage}
                     alt="logo"
                     width={900}
                     height={900}
