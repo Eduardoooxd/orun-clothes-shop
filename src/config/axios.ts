b4dcc91b9d9eae96004c782f17c275d93fbb72a9
Eduardoooxd <ecouto93@gmail.com>
2023-06-12 15:08:35 +0100

    chore: trying new approach on root url

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,6 @@
+import { createRootUrl } from '@/lib/utils';
 import axios from 'axios';
 
 export const axiosBase = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_ROOT_URL}`,
+    baseURL: `${createRootUrl()}`,
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,5 +14,14 @@ export const createUrl = (pathname: string, params: URLSearchParams | ReadonlyUR
 };
 
 export const createApiUrl = (relativePath: string): string => {
-    return `${process.env.NEXT_PUBLIC_ROOT_URL}${relativePath}`;
+    return `${createRootUrl()}${relativePath}`;
+};
+
+export const createRootUrl = (): string => {
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    }
+    return process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'
+        : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 };
