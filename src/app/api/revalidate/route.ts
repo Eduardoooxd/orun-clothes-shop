c7756eb23f73978296c37cb9734888af20ddb4d7
Eduardoooxd <ecouto93@gmail.com>
2023-06-14 22:38:36 +0100

    chore: Changing validation of revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,32 +1,11 @@
 import { TAGS } from '@/config/shopifyConfig';
 import { revalidateTag } from 'next/cache';
-import { headers } from 'next/headers';
-import { NextRequest, NextResponse } from 'next/server';
-
-export const runtime = 'edge';
+import { NextResponse } from 'next/server';
 
 // We always need to respond with a 200 status code to Shopify,
 // otherwise it will continue to retry the request.
-export async function POST(req: NextRequest): Promise<Response> {
-    const productWebhooks = ['products/create', 'products/delete', 'products/update'];
-    const topic = headers().get('x-shopify-topic') || 'unknown';
-    const secret = req.nextUrl.searchParams.get('secret');
-    const isProductUpdate = productWebhooks.includes(topic);
-
-    if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
-        console.log(`Secret is ${secret}`);
-        console.error('Invalid revalidation secret.');
-        return NextResponse.json({ status: 200 });
-    }
-
-    if (!isProductUpdate) {
-        // We don't need to revalidate anything for any other topics.
-        return NextResponse.json({ status: 200 });
-    }
-
-    if (isProductUpdate) {
-        revalidateTag(TAGS.products);
-    }
+export async function POST(): Promise<Response> {
+    revalidateTag(TAGS.products);
 
     return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
 }
