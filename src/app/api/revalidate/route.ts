7b463731f75a18b2b7b349bdc8caf9221a0f347b
Eduardoooxd <ecouto93@gmail.com>
2023-06-14 22:49:37 +0100

    chore: Making revalidate wait before caching content again

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,10 +1,13 @@
 import { TAGS } from '@/config/shopifyConfig';
+import { timeout } from '@/lib/utils';
 import { revalidateTag } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 // We always need to respond with a 200 status code to Shopify,
 // otherwise it will continue to retry the request.
 export async function POST(): Promise<Response> {
+    // We need to wait a time to ensure that the graphql API data is updated, before we can cache it again.
+    await timeout(60000);
     revalidateTag(TAGS.products);
 
     return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
